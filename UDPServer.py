def3aebc8ed85e5f9a43ceb827a19c361d3c93cc
leofreua <leonardo.batista.freua@gmail.com>
2017-10-30 04:30:57 -0200

    Removed tab usage error

--- UDPServer.py
+++ UDPServer.py
@@ -8,9 +8,9 @@ import time
 class UDPServerConnection(object):
 
     def __init__(self, host, port):
-		self.host = host
-		self.port = port
-		self.content_dir = '/' #Directory where webpage files are stored
+        self.host = host
+        self.port = port
+        self.content_dir = '/home/lfreua/Downloads' #Directory where webpage files are stored
 
     def run(self):
 
@@ -48,26 +48,24 @@ class UDPServerConnection(object):
 
     def _generateHeaders(self, code):
 
-		"""
-		Generate HTTP response headers.
+        """	Generate HTTP response headers.
 		Parameters:
 			- response_code: HTTP response code to add to the header.
 		"""
-		headerCode = ''
-		h = ''
+        headerCode = ''
+        h = ''
+        if (code == 200):
+            headerCode = 'HTTP/1.1 200 OK\n'
+        elif(code == 404):
+            headerCode = 'HTTP/1.1 404 Not Fund\n'
 
-		if (code == 200):
-			headerCode = 'HTTP/1.1 200 OK\n'
-		elif(code == 404):
-			headerCode = 'HTTP/1.1 404 Not Fund\n'
+        current_date = time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime())
 
-		current_date = time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime())
+        h += 'Date: ' + current_date + '\n'
+        h += 'Server: HTTP-Server\n'
+        h += 'Connection: close\n\n'
 
-		h += 'Date: ' + current_date + '\n'
-		h += 'Server: HTTP-Server\n'
-		h += 'Connection: close\n\n'
-
-		return h
+        return h
 
     def shutDown(self):
         try:
@@ -77,61 +75,61 @@ class UDPServerConnection(object):
             print("Warning could not shut down the socket. Maybe it was already closed? " + e)
 
     def _headleToClient(self, client, address):
-		"""
+        """
 		Intructions that will handle the incoming connections and send the requrested file from content_dir
 		Parameters:
 			- client: socket client from accept()
 			- address: socket address from accept()
 		"""
-		PACKET_SIZE = 1024
+        PACKET_SIZE = 1024
 
-		while True:
+        while True:
 			#print("Client: " self.client)
-			data = client.recv(PACKET_SIZE).decode() #Receive data packet from client and decode
+            data = client.recv(PACKET_SIZE).decode() #Receive data packet from client and decode
 
-			if not data: break
+            if not data: break
 
-			request_method = data.split(' ')[0]
-			print("Method: {m}".format(m=request_method))
-			print("Request Body: {b}".format(b=data))
+            request_method = data.split(' ')[0]
+            print("Method: {m}".format(m=request_method))
+            print("Request Body: {b}".format(b=data))
 
-			response = ''
-			if request_method == "GET" or request_method == "HEAD":
-				#Ex.: "GET /index.html" split on space
-				file_requested = data.split(' ')[1]
+            response = ''
+            if request_method == "GET" or request_method == "HEAD":
+        		#Ex.: "GET /index.html" split on space
+                file_requested = data.split(' ')[1]
 
-				#if get has parameters ('?'), ignore them
-				file_requested = file_requested.split('?')[0]
+        		#if get has parameters ('?'), ignore them
+                file_requested = file_requested.split('?')[0]
 
-				if  file_requested == "/":
-					file_requested = "/teste"
+                if  file_requested == "/":
+                    file_requested = "/teste"
 
-				filepath_to_send = self.content_dir + file_requested
-				print("Serving web page [{wp}]".format(wp=filepath_to_send))
+                filepath_to_send = self.content_dir + file_requested
+                print("Serving web page [{wp}]".format(wp=filepath_to_send))
 
-				#Load and Serve files content
-				try:
-					f = open(filepath_to_send, 'rb')
-					if request_method == "GET": #Read only for GET
-						response_data = f.read()
-					f.close()
-					response_header = self._generateHeaders(200)
-				except Exception as e:
-					print("File not found. Sending 404 page.")
-					response_header = self._generateHeaders(404)
+        		#Load and Serve files content
+                try:
+                    f = open(filepath_to_send, 'rb')
+                    if request_method == "GET": #Read only for GET
+                        response_data = f.read()
+                    f.close()
+                    response_header = self._generateHeaders(200)
+                except Exception as e:
+                    print("File not found. Sending 404 page.")
+                    response_header = self._generateHeaders(404)
 
-					if request_method == "GET": #Temporary 404 response Page
-						response_data = b"<html><body><center><h1>Error 404: FIle not found</h1></center></body></html>"
+                    if request_method == "GET": #Temporary 404 response Page
+                        response_data = b"<html><body><center><h1>Error 404: FIle not found</h1></center></body></html>"
 
-			response = response_header.encode()
-			if request_method == "GET":
-				response += response_data
+            response = response_header.encode()
+            if request_method == "GET":
+                response += response_data
 
-			client.send(response)
-			client.close()
-			break
-		else:
-			print("Unknown HTTP request method: {method}".format(method=request_method))
+            client.send(response)
+            client.close()
+            break
+        else:
+            print("Unknown HTTP request method: {method}".format(method=request_method))
 if __name__ == '__main__':
     server = UDPServerConnection('127.0.0.1', 999)
     server.run()
