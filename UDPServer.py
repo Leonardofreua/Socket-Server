c992b0dd50761a122cd1d574afe3f2ca1493c909
leofreua <leonardo.batista.freua@gmail.com>
2017-11-02 19:37:47 -0200

    Changed the concatenation of the headers and added the Content Length in it.

--- UDPServer.py
+++ UDPServer.py
@@ -5,12 +5,15 @@ import threading
 import signal
 import time
 
-class UDPServerConnection(object):
+#http://blog.wachowicz.eu/?p=256
+#https://gist.github.com/joncardasis/cc67cfb160fa61a0457d6951eff2aeae
 
-    def __init__(self, host, port):
+class UDPServerConnection:
+
+    def __init__(self, host = '127.0.0.1', port=8080):
         self.host = host
         self.port = port
-        self.content_dir = '/' #Directory where webpage files are stored
+        self.content_dir = os.path.dirname(os.path.abspath(__file__)) #Directory where webpage files are stored
 
     def run(self):
 
@@ -30,6 +33,7 @@ class UDPServerConnection(object):
             print("Starting server on {host}:{port}".format(host=self.host, port=self.port))
             self.sock.bind((self.host, self.port))
             print("Server started on port {port}.".format(port=self.port))
+
         except socket.error as e:
             print ('Bind failed:' + str(e[0]) + 'Message ' + e[1])
             self.shutDown()
@@ -46,26 +50,26 @@ class UDPServerConnection(object):
             print("Client connected from {addr}".format(addr=address))
             threading.Thread(target=self._headleToClient, args=(client, address)).start()
 
-    def _generateHeaders(self, code):
-
-        """	Generate HTTP response headers.
+    def _generateHeaders(self, code, filePath):
+        """
+		Generate HTTP response headers.
 		Parameters:
 			- response_code: HTTP response code to add to the header.
 		"""
         headerCode = ''
-        h = ''
+
         if (code == 200):
-            headerCode = 'HTTP/1.1 200 OK\n'
+            headerCode += 'HTTP/1.1 200 OK\n'
         elif(code == 404):
-            headerCode = 'HTTP/1.1 404 Not Fund\n'
+            headerCode += 'HTTP/1.1 404 Not Fund\n'
 
         current_date = time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime())
 
-        h += 'Date: ' + current_date + '\n'
-        h += 'Server: HTTP-Server\n'
-        h += 'Connection: close\n\n'
+        headerCode += 'Date: ' + current_date + '\n'
+        headerCode += 'Server: HTTP-Server\n'
+        headerCode += 'Connection: close\n'
 
-        return h
+        return headerCode
 
     def shutDown(self):
         try:
@@ -84,7 +88,6 @@ class UDPServerConnection(object):
         PACKET_SIZE = 1024
 
         while True:
-			#print("Client: " self.client)
             data = client.recv(PACKET_SIZE).decode() #Receive data packet from client and decode
 
             if not data: break
@@ -94,34 +97,42 @@ class UDPServerConnection(object):
             print("Request Body: {b}".format(b=data))
 
             response = ''
+
             if request_method == "GET" or request_method == "HEAD":
-        		#Ex.: "GET /index.html" split on space
+				#Ex.: "GET /index.html" split on space
                 file_requested = data.split(' ')[1]
 
-        		#if get has parameters ('?'), ignore them
+				#if get has parameters ('?'), ignore them
                 file_requested = file_requested.split('?')[0]
 
                 if  file_requested == "/":
                     file_requested = "/teste"
 
                 filepath_to_send = self.content_dir + file_requested
-                print("Serving web page [{wp}]".format(wp=filepath_to_send))
+                print("Sending web page [{wp}]".format(wp=filepath_to_send))
 
-        		#Load and Serve files content
+                contentSize = 0
+
+				#Load and Send files content
                 try:
                     f = open(filepath_to_send, 'rb')
                     if request_method == "GET": #Read only for GET
                         response_data = f.read()
                     f.close()
-                    response_header = self._generateHeaders(200)
+                    response_header = self._generateHeaders(200, filepath_to_send)
+                    contentSize = os.stat(filepath_to_send).st_size
+
                 except Exception as e:
                     print("File not found. Sending 404 page.")
-                    response_header = self._generateHeaders(404)
+                    response_header = self._generateHeaders(404, filepath_to_send)
 
                     if request_method == "GET": #Temporary 404 response Page
                         response_data = b"<html><body><center><h1>Error 404: FIle not found</h1></center></body></html>"
+                        contentSize = str(sys.getsizeof(response_data))
 
             response = response_header.encode()
+            response += 'Content Length: {fileSize}\n\n'.format(fileSize=contentSize)
+
             if request_method == "GET":
                 response += response_data
 
@@ -130,6 +141,7 @@ class UDPServerConnection(object):
             break
         else:
             print("Unknown HTTP request method: {method}".format(method=request_method))
+
 if __name__ == '__main__':
-    server = UDPServerConnection('127.0.0.1', 999)
-    server.run()
+	server = UDPServerConnection()
+	server.run()
